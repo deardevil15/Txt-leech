b224cd0a3cc97d53745d1a5af38fa5959affcc53
deardevil15 <157885783+deardevil15@users.noreply.github.com>
2024-03-04 13:08:58 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28929701"
-api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
-bot_token = "6893027676:AAEytAdJL-5uY_2LqeCVWA7ziogwPvxBZQw"
+api_id = "22533478"
+api_hash = "e88e19343c3b8e223911fea0b17b3ffc"
+bot_token = "6420652584:AAF6m097SOlaQWIW0RnDqRyNyavlkUtILng"
